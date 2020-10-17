29233469194073d33c262ec7d847dc284a521a77
isehgal2 <isehgal2@illinois.edu>
2020-10-17 18:46:47 -0500

    Completed citation maker

--- Database/neo4j-driver.py
+++ Database/neo4j-driver.py
@@ -38,51 +38,61 @@ class PaperNetApp:
 			MERGE (a:Author {name: author})
 			MERGE (a)<-[w:WRITTEN]->(p)
 			''')
-		print(i)
+		# print(i)
 		result = tx.run(query, i=index)
 		return result
 
 		#  first_upload_date: $first_upload_date, update_date: $update_date })
 
 
-	# Creating the paper nodes
-	def create_paper(self, paper_info):
-		with self.driver.session() as session:
-			# Write transactions allow the driver to handle retries and transient errors
-			result = session.write_transaction(self._create_and_return_paper, paper_info)
-			return result
+	# # Creating the paper nodes
+	# def create_paper(self, paper_info):
+	# 	with self.driver.session() as session:
+	# 		# Write transactions allow the driver to handle retries and transient errors
+	# 		result = session.write_transaction(self._create_and_return_paper, paper_info)
+	# 		return result
 
-	@staticmethod
-	def _create_and_return_paper(tx, paper_info):
-		query = (
-			"CREATE (p:Paper { id: $id, \
-				submitter: $submitter, authors: $authors, \
-				title: $title, comments: $comments, \
-				journal_ref: $journal_ref, doi: $doi \
-				abstract: $abstract, categories: $categories, \
-				first_upload_date: $first_upload_date, update_date: $update_date })"
-			"RETURN p"
-		)
-		result = tx.run(query, id=paper_info.id, submitter = paper_info.submitter, authors = paper_info.authors, title = paper_info.title, \
-				comments = paper_info.comments, journal_ref = paper_info.journal_ref,  doi = paper_info.doi, abstract =  paper_info.abstract, \
-				categories = paper_info.categories, first_upload_date = paper_info.first_upload_date, update_date = paper_info.update_date)
-		return result
+	# @staticmethod
+	# def _create_and_return_paper(tx, paper_info):
+	# 	query = (
+	# 		"CREATE (p:Paper { id: $id, \
+	# 			submitter: $submitter, authors: $authors, \
+	# 			title: $title, comments: $comments, \
+	# 			journal_ref: $journal_ref, doi: $doi \
+	# 			abstract: $abstract, categories: $categories, \
+	# 			first_upload_date: $first_upload_date, update_date: $update_date })"
+	# 		"RETURN p"
+	# 	)
+	# 	result = tx.run(query, id=paper_info.id, submitter = paper_info.submitter, authors = paper_info.authors, title = paper_info.title, \
+	# 			comments = paper_info.comments, journal_ref = paper_info.journal_ref,  doi = paper_info.doi, abstract =  paper_info.abstract, \
+	# 			categories = paper_info.categories, first_upload_date = paper_info.first_upload_date, update_date = paper_info.update_date)
+	# 	return result
 
 
 	# Creating the citations connections
-	def create_paper_citation(self, paper, papers_cited):
+	def create_paper_citation(self):
 		with self.driver.session() as session:
 			# Write transactions allow the driver to handle retries and transient errors
-			result = session.write_transaction(
-				self._create_and_return_citation, paper, papers_cited)
-			# for record in result:
-			#     print("Created citation between: {p1}, {p2}".format(
-			#         p1=record['p1'], p2=record['p2']))
+			with open("test_output.txt") as file:
+				for line in file:
+					paper = line[:8]
+					cited_papers = line[line.find('[')+1:-1]
+					cited_papers = cited_papers.replace("\'", "")
+					print(paper, cited_papers)
+					result = session.write_transaction(self._create_and_return_citation, paper, cited_papers)
 
 	@staticmethod
 	def _create_and_return_citation(tx, paper, papers_cited):
 		# TODO: Create citation relationship between the paper and each of the papers_cited
-		pass
+		query = (
+			'''
+			WITH $papers_cited as y
+			UNWIND split(y, ',') AS x
+			MERGE (p {id: $paper})-[:CITED]->(m {id: x})
+			'''
+		)
+		result = tx.run(query, paper=paper, papers_cited=papers_cited)
+		return result
 	
 
 	def find_paper(self, paper_name):
@@ -100,24 +110,12 @@ class PaperNetApp:
 		)
 		result = tx.run(query, paper_name=paper_name)
 		return [record["name"] for record in result]
-
-	# def find_papers_citatons(self, paper_name):
-	#      with self.driver.session() as session:
-	#         result = session.read_transaction(self._find_and_return_papers_citations, paper_name)
-	#         for record in result:
-	#             print("Found paper: {record}".format(record=record))
-
-	# @staticmethod
-	# def _find_and_return_papers_citations(tx, paper_name): 
-	#      query = (
-	#         "MATCH (p1:Paper {title: $paper_name})-[:CITED]->(p2:Paper)"
-	#         "RETURN p2.name AS name"
-	#     )
-	#     result = tx.run(query, paper_name=paper_name)
-	#     return [record["name"] for record in result]
 		
 
-for i in range(0, 251000, 1000):
-	paper = PaperNetApp("neo4j://104.198.183.32", "neo4j", "tSgiHA1DN9R98PeM")
-	paper.populate_paper(index=i)
-	paper.close()
+# for i in range(0, 251000, 1000):
+# 	paper = PaperNetApp("neo4j://104.198.183.32", "neo4j", "tSgiHA1DN9R98PeM")
+# 	paper.populate_paper(index=i)
+# 	paper.close()
+
+paper = PaperNetApp("neo4j://104.198.183.32", "neo4j", "tSgiHA1DN9R98PeM")
+paper.create_paper_citation()
